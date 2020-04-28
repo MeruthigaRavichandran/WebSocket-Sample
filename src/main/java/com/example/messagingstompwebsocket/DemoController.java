51f6a8334c78a85a1020ecabc4b911dd897aaf32
iamvickyav <vickyavw.10@gmail.com>
2020-04-28 13:53:41 +0530

    changes type from JSON to String

--- src/main/java/com/example/messagingstompwebsocket/DemoController.java
+++ src/main/java/com/example/messagingstompwebsocket/DemoController.java
@@ -3,7 +3,10 @@ package com.example.messagingstompwebsocket;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.support.GenericMessage;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,7 +22,11 @@ public class DemoController {
 
     @RequestMapping("/test")
     ResponseEntity getMessage(@RequestParam("name") String name) {
-        template.convertAndSend("/topic/greetings", new Greeting(name));
+
+        //template.convertAndSend("/topic/greetings", new Greeting(name));
+
+        template.convertAndSend("/topic/greetings", name);
+
         Map<String, String> map = new HashMap<>();
         map.put("message", "Message sent in websocket");
         return new ResponseEntity<>(map, HttpStatus.OK);
